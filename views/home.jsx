93e8999167d5736d4f4f744df5b2689b02a622d4
unknown <saadiqgp@gmail.com>
2022-08-28 15:34:38 -0400

    adding and rendering a new view

--- views/home.jsx
+++ views/home.jsx
@@ -5,7 +5,7 @@ function home() {
   return (
     <Def>
       <main>
-        <h1>HOME</h1>
+        <h1>REST-Rant</h1>
         <a href="/places">
           <button className="btn-primary">Places Page</button>
         </a>
