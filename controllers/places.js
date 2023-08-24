cecb92bb8d00b55aa63fd120aec8de9378a4a86f
numbafye <saadiqgp@gmail.com>
2023-08-24 08:09:39 -0400

    delete route in controller redirects to places

--- controllers/places.js
+++ controllers/places.js
@@ -39,4 +39,17 @@ router.post("/", (req, res) => {
   res.redirect("/places");
 });
 
+//DELETE ROUTE
+router.delete("/:id", (req, res) => {
+  let id = Number(req.params.id);
+  if (isNaN(id)) {
+    res.render("error404");
+  } else if (!places[id]) {
+    res.render("error404");
+  } else {
+    places.splice(id, 1);
+    res.redirect("/places");
+  }
+});
+
 module.exports = router;
