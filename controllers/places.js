d49ea71f0de1f518925450453f256dda923ca01f
numbafye <saadiqgp@gmail.com>
2023-08-26 18:09:02 -0400

    added the edit form

--- controllers/places.js
+++ controllers/places.js
@@ -39,6 +39,18 @@ router.post("/", (req, res) => {
   res.redirect("/places");
 });
 
+//EDIT ROUTE
+router.get("/:id/edit", (req, res) => {
+  let id = Number(req.params.id);
+  if (isNaN(id)) {
+    res.render("error404");
+  } else if (!places[id]) {
+    res.render("error404");
+  } else {
+    res.render("places/edit", { place: places[id] });
+  }
+});
+
 //DELETE ROUTE
 router.delete("/:id", (req, res) => {
   let id = Number(req.params.id);
